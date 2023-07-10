c8e6aa6c37b0b2edcbb9bf53c111fe57ec7dbb30
Leandro_Vitor <leandrovitor455@gmail.com>
2023-07-10 18:05:24 -0300

    New font and the end of my progress for today

--- test.py
+++ test.py
@@ -3,7 +3,11 @@ from sys import exit
 
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Projetinho')
+clock = pygame.time.Clock()
+test_surface = pygame.Surface((100,200))
+test_surface.fill((255,0,0))
 
 while True:
     #draw everything
@@ -12,5 +16,8 @@ while True:
           if event.type == pygame.QUIT:
                pygame.quit()
                exit()
+    screen.blit(test_surface,(200,100))
+
     pygame.display.update()
+    clock.tick(60)
     
